1d9bd7d4dd33446f00ab2aca1363a58d2386fb0b
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-13 11:21:02 +0330

    Update Startup.cs

--- src/InternetBank/Startup.cs
+++ src/InternetBank/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +32,8 @@ namespace InternetBank
             services.AddIdentity<ApplicationUser, IdentityRole<int>>()
                     .AddEntityFrameworkStores<ApplicationDbConext>()
                     .AddDefaultTokenProviders();
+            services.AddDbContext<ApplicationDbConext>(options =>
+                     options.UseNpgsql(Configuration.GetConnectionString("ApplicationConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
