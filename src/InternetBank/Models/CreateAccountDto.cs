0df2875bd129b1eb03df2db1a19e39d086416e7a
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-14 08:47:27 +0330

    Add CreateAccountDto

---
+++ src/InternetBank/Models/CreateAccountDto.cs
@@ -0,0 +1,9 @@
+namespace InternetBank.Models
+{
+    public class CreateAccountDto
+    {
+        public int Type { get; set; }
+        [Range(10000 , long.MaxValue)]
+        public int Amount { get; set; }
+    }
+}
