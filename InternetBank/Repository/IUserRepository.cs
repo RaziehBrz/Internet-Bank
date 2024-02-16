59259978054236d9697ec3a224a0fc223b982529
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-16 11:01:26 +0330

    Add Login & Register services

---
+++ InternetBank/Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using InternetBank.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace InternetBank.Repository
+{
+    public interface IUserRepository
+    {
+        Task<IdentityResult> SignUp(SignUpDto signUpDto);
+        Task<string> Login(LoginDto loginDto);
+    }
+}
