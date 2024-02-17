53203790bc426289171affa3db47befaf40e0a74
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-17 09:55:34 +0330

    Update UserRepository

--- InternetBank/Repository/UserRepository.cs
+++ InternetBank/Repository/UserRepository.cs
@@ -14,59 +14,54 @@ namespace InternetBank.Repository
 {
     public class UserRepository : IUserRepository
     {
-        private UserManager<ApplicationUser> _userManager { get; set; }
-        private SignInManager<ApplicationUser> _signInManager { get; set; }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly IConfiguration _configuration;
-        public UserRepository(
-            UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager,
-            IConfiguration configuration)
+
+        public UserRepository(UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager,
+        IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _configuration = configuration;
         }
 
-        //Register
         public async Task<IdentityResult> SignUp(SignUpDto signUpDto)
         {
             var user = new ApplicationUser()
             {
                 FirstName = signUpDto.FirstName,
                 LastName = signUpDto.LastName,
-                NationalCode = signUpDto.NationalCode,
-                BirthDate = signUpDto.BirthDate,
-                PhoneNumber = signUpDto.PhoneNumber,
                 Email = signUpDto.Email,
                 UserName = signUpDto.Email
             };
             return await _userManager.CreateAsync(user, signUpDto.Password);
         }
-        //Login
         public async Task<string> Login(LoginDto loginDto)
         {
-            var result = await _signInManager.PasswordSignInAsync(loginDto.UserName, loginDto.Password, false, false);
+            var result = await _signInManager.PasswordSignInAsync(loginDto.Email, loginDto.Password, false, false);
             if (!result.Succeeded) return null;
 
             var authClaims = new List<Claim>()
             {
-                new Claim(ClaimTypes.Email , loginDto.UserName) ,
+                new Claim(ClaimTypes.Email , loginDto.Email) ,
                 new Claim(JwtRegisteredClaimNames.Jti , Guid.NewGuid().ToString())
             };
 
-            var authSignInKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var authSignInKey = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(_configuration["JWT:Secret"]));
 
             var token = new JwtSecurityToken(
-            issuer: _configuration["JWT:ValidIssuer"],
-            audience: _configuration["JWT:ValidAudience"],
-            expires: DateTime.Now.AddDays(1),
-            claims: authClaims,
-            signingCredentials: new SigningCredentials(authSignInKey, SecurityAlgorithms.HmacSha256Signature)
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddDays(1),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSignInKey, SecurityAlgorithms.HmacSha256Signature)
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
-
 
     }
 }
