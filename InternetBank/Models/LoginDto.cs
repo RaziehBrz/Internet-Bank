c6648cc078986f2cca9bd2456b5e911aab40a0f0
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-17 09:56:46 +0330

    Update LoginDto

--- InternetBank/Models/LoginDto.cs
+++ InternetBank/Models/LoginDto.cs
@@ -5,7 +5,7 @@ namespace InternetBank.Models
     public class LoginDto
     {
         [Required(ErrorMessage = "لطفا نام کاربری را وارد کنید!"), EmailAddress]
-        public string UserName { get; set; }
+        public string Email { get; set; }
         [Required(ErrorMessage = "لطفا رمزعبور را وارد کنید!")]
         public string Password { get; set; }
     }
