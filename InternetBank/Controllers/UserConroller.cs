34978512ab145bced19d4bbe1dc3375a7a2a31f6
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-20 12:37:07 +0330

    Update Claims List

--- InternetBank/Controllers/UserConroller.cs
+++ InternetBank/Controllers/UserConroller.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetBank.Data;
 using InternetBank.Models;
 using InternetBank.Repository;
 using Microsoft.AspNetCore.Mvc;
